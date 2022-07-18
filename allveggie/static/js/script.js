30371536e8282b7329ef050292eb5df90080f204
CarylThom <cat.twnti@gmail.com>
2022-07-18 15:25:28 +0000

    Move static files to allveggie app

---
+++ allveggie/static/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  // collapsible initializataion
+  let collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
+
+  // modal initialization
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+});
