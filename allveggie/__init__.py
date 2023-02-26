a051f66ec211eea153121931cf0c2b73ada7cbf5
CarylThom <cat.twnti@gmail.com>
2023-02-26 22:01:11 +0000

    Changes to init file

--- allveggie/__init__.py
+++ allveggie/__init__.py
@@ -6,19 +6,18 @@ from flask_migrate import Migrate
 if os.path.exists("env.py"):
     import env  # noqa
 
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["ELEPHANTSQL_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["ELEPHANTSQL_DATABASE_URI"] = uri  # heroku
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
-db = ElephantSQL(app)
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from allveggie import routes  # noqa
