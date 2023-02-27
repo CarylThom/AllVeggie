bbea1bbfbbf66cb493c4a2369452ba1556513081
CarylThom <cat.twnti@gmail.com>
2023-02-27 10:25:31 +0000

    Remove Flask-Migrate requirements

--- allveggie/__init__.py
+++ allveggie/__init__.py
@@ -2,7 +2,6 @@ import os
 import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -18,6 +17,5 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 from allveggie import routes  # noqa
